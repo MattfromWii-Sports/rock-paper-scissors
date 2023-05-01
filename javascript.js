a94f7c7e413ca9329209076ae8e103c0f4956a79
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-05-01 21:00:02 +0800

    Fix And Simplify Round Evaluation Function

--- javascript.js
+++ javascript.js
@@ -3,23 +3,28 @@ function getComputerChoice() {
     let randomN = Math.floor(Math.random() * 3);
     if (randomN == 0) {
         computerSelection = 'Rock';
+        return 0;
     } else if (randomN == 1){
         computerSelection = 'Paper';
+        return 1;
     } else if (randomN == 2){
         computerSelection = 'Scissors';
+        return 2;
     }
-    console.log(computerSelection);
 }
 
 //User Choice
 function getUserChoice() {
     let userPrompt = prompt('Rock, Paper, or Scissors?').toLowerCase();
     if (userPrompt == 'rock') {
-        userSelection = 'Rock';  
+        userSelection = 'Rock';
+        return 0;
     } else if (userPrompt == 'paper') {
-        userSelection = 'Paper';  
+        userSelection = 'Paper';
+        return 1;
     } else if (userPrompt == 'scissors' || userPrompt == 'scissor') {
-        userSelection = 'Scissors'; 
+        userSelection = 'Scissors';
+        return 2;
     } else {
         alert ('Please type either rock, paper, or scissors');
         getUserChoice();
@@ -28,17 +33,14 @@ function getUserChoice() {
 
 //Round
 function roundEval() {
-    getComputerChoice();
-    getUserChoice();
+    let computerNum = getComputerChoice();
+    let userNum = getUserChoice();
     console.log(`You chose ${userSelection}!`)
     console.log(`The Computer chose ${computerSelection}!`);
-    if (
-    userSelection == 'Rock' && computerSelection == 'Scissors' || 
-    userSelection == 'Paper' && computerSelection == 'Rock' || 
-    userSelection == 'Scissors' && computerSelection == 'paper' ){
+    if (userNum - computerNum == 1 || userNum - computerNum == -2) {
         console.log('You Win!')
         return 1;
-    } else if (userSelection == computerSelection) {
+    } else if (userNum - computerNum == 0) {
         console.log('Its a Tie!')
         return 0;
     } else {
