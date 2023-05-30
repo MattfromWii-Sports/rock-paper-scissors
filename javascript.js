eaf8b16bdee7f7eda66780d8ab8c9f8166f11392
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-05-30 20:44:17 +0800

    Create Function That Picks Randomly Between The Three Choices, And Stores The Computer's Choices In Variables

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 //Variables And Elements In Main
 let typeValue;
 let playerTypeName;
-const computerChoiceText = document.querySelector('.computer-choice.text');
+let computerValue;
+let computerTypeName;
 const resultText = document.querySelector('.result.text');
 const roundText = document.querySelector('.tally.text');
 
@@ -9,19 +10,33 @@ const roundText = document.querySelector('.tally.text');
 const playerChoices = document.querySelectorAll('.left-column > .box');
 playerChoices.forEach(btn => btn.addEventListener('click', round));
 function round() {
-    playerTypeValue = this.dataset.value; /*For round calculations*/
+    const currentPChoice = this;
+    playerTypeValue = currentPChoice.dataset.value; /*For round calculations*/
+    playerTypeName = currentPChoice.dataset.type; /*For player text */
     console.log(playerTypeValue);
-    playerTypeName = this.dataset.type; /*For player text */
     console.log(playerTypeName);
-    this.classList.add('highlight');
+    const currentCChoice = randomComputer();
+    computerTypeValue = currentCChoice.dataset.valueCpu; /*For round calculations*/
+    computerTypeName = currentCChoice.dataset.typeCpu; /*For player text */
+    console.log(computerTypeValue);
+    console.log(computerTypeName);
+    currentPChoice.classList.add('highlight');
     playerChoices.forEach(btn => btn.removeEventListener('click', round));
     setTimeout(playerChoiceF, 200);
 }
 
 //Computer values, calculation
+function randomComputer() {
+    let randomN = Math.floor(Math.random() * 3);
+    return document.querySelector(`[data-value-cpu="${randomN}"]`);
+}
 
 //text timeout functions
 function playerChoiceF() {
     const playerChoiceText = document.querySelector('.player-choice.text');
     playerChoiceText.textContent = `You chose ${playerTypeName}`;
+}
+function computerChoiceF() {
+    const computerChoiceText = document.querySelector('.computer-choice.text');
+    computerChoiceText.textContent = `You chose ${computerTypeName}`;
 }
