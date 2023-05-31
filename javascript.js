5f0f82de1cd0394fa258153e4d595ffb88b51362
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-05-31 13:38:37 +0800

    Add Function That Calculates Winner And Displays Results, And Fix Bug For Different Computer Choice Values

--- javascript.js
+++ javascript.js
@@ -3,30 +3,32 @@ let typeValue;
 let playerTypeName;
 let computerValue;
 let computerTypeName;
-const resultText = document.querySelector('.result.text');
-const roundText = document.querySelector('.tally.text');
+let result;
+let score;
+let calculateWinner;
+let currentPChoice;
 
 //Player Choice event listener, Main Function
 const playerChoices = document.querySelectorAll('.left-column > .box');
 playerChoices.forEach(btn => btn.addEventListener('click', round));
 function round() {
-    const currentPChoice = this;
+    currentPChoice = this;
+    currentCChoice = randomComputer();
     playerTypeValue = currentPChoice.dataset.value; /*For round calculations*/
     playerTypeName = currentPChoice.dataset.type; /*For player text */
-    console.log(playerTypeValue);
-    console.log(playerTypeName);
-    const currentCChoice = randomComputer();
     computerTypeValue = currentCChoice.dataset.valueCpu; /*For round calculations*/
     computerTypeName = currentCChoice.dataset.typeCpu; /*For player text */
-    console.log(computerTypeValue);
-    console.log(computerTypeName);
+    console.log(playerTypeValue);
+    console.log(computerTypeValue); 
+    calculateWinner = parseInt(playerTypeValue) - parseInt(computerTypeValue);
     currentPChoice.classList.add('highlight');
     playerChoices.forEach(btn => btn.removeEventListener('click', round));
     playerText();
     computerTextBox();
+    roundResult();
 }
 
-//Computer values
+//Calculations; Computer And Results
 function randomComputer() {
     let randomN = Math.floor(Math.random() * 3);
     return document.querySelector(`[data-value-cpu="${randomN}"]`);
@@ -40,7 +42,20 @@ function playerText() {
 function computerTextBox() {
     setTimeout(function() {
     const computerChoiceText = document.querySelector('.computer-choice.text');
-    randomComputer().classList.add('highlight');
+    currentCChoice.classList.add('highlight');
     computerChoiceText.textContent = `The Computer chose ${computerTypeName}`;
     }, 900)
 }
+function roundResult() {
+    setTimeout(function() {
+    const resultText = document.querySelector('.result.text');
+    if (calculateWinner == 0) {
+        resultText.textContent = 'Its A Tie';
+    } else if (calculateWinner == 1 || calculateWinner == -2) {
+        resultText.textContent = `${playerTypeName} beats ${computerTypeName}, You Win!`;
+    } else {
+        resultText.textContent = `${computerTypeName} beats ${playerTypeName}, You Lose!`;
+    }
+    }, 1800)
+}
+
