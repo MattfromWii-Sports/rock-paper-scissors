233228e7d4de9a30a8c6b9ae35542a18e2df87d5
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-06-01 15:22:15 +0800

    Combine Text Functions Into One Function

--- javascript.js
+++ javascript.js
@@ -3,6 +3,8 @@ let typeValue;
 let playerTypeName;
 let computerValue;
 let computerTypeName;
+let currentCChoice;
+let currentPChoice;
 let playerPoints = 0;
 let computerPoints = 0;
 const playerChoiceText = document.querySelector('.player-choice.text');
@@ -18,18 +20,14 @@ playerChoices.forEach(btn => btn.addEventListener('click', round));
 
 //1 Round
 function round() {
-    let currentPChoice = this;
-    currentCChoice = randomComputer();
     playerChoices.forEach(btn => btn.removeEventListener('click', round));
+    currentPChoice = this; //Current Player div
+    currentCChoice = randomComputer(); //Current Computer div
     playerTypeValue = currentPChoice.dataset.value; /*For round calculations*/
-    playerTypeName = currentPChoice.dataset.type; /*For player text */
-    currentPChoice.classList.add('highlight');
-    playerText();
+    playerTypeName = currentPChoice.dataset.type; /*For player text */ 
     computerTypeValue = currentCChoice.dataset.valueCpu; /*For round calculations*/
-    computerTypeName = currentCChoice.dataset.typeCpu; /*For player text */
-    computerTextBox();
-    roundResult();
-    scoreTally();
+    computerTypeName = currentCChoice.dataset.typeCpu; /*For player text */ 
+    textPCRT();
 }
 
 //Calculations; Computer And Results
@@ -39,31 +37,25 @@ function randomComputer() {
 }
 
 //text functions & Timeout
-function playerText() {
+function textPCRT() {
     playerChoiceText.textContent = `You chose ${playerTypeName}`;
-}
-function computerTextBox() {
+    currentPChoice.classList.add('highlight');
     setTimeout(function() {
-    currentCChoice.classList.add('highlight');
-    computerChoiceText.textContent = `The Computer chose ${computerTypeName}`;
+        currentCChoice.classList.add('highlight');
+        computerChoiceText.textContent = `The Computer chose ${computerTypeName}`;
     }, 1400)
-}
-function roundResult() {
     setTimeout(function() {
-    if (parseInt(playerTypeValue) == parseInt(computerTypeValue)) {
-        resultText.textContent = 'Its A Tie!';
-    } else if (parseInt(playerTypeValue) - parseInt(computerTypeValue) == 1 || parseInt(playerTypeValue) - parseInt(computerTypeValue) == -2) {
-        resultText.textContent = `${playerTypeName} beats ${computerTypeName}, You Win!`;
-        playerPoints += 1;
-    } else {
-        resultText.textContent = `${computerTypeName} beats ${playerTypeName}, You Lose!`;
-        computerPoints += 1;
-    }
+        if (parseInt(playerTypeValue) == parseInt(computerTypeValue)) {
+            resultText.textContent = 'Its A Tie!';
+        } else if (parseInt(playerTypeValue) - parseInt(computerTypeValue) == 1 || parseInt(playerTypeValue) - parseInt(computerTypeValue) == -2) {
+            resultText.textContent = `${playerTypeName} beats ${computerTypeName}, You Win!`;
+            playerPoints += 1;
+        } else {
+            resultText.textContent = `${computerTypeName} beats ${playerTypeName}, You Lose!`;
+            computerPoints += 1;
+        }
     }, 2200)
-}
-function scoreTally() {
     setTimeout(function() {  
         tally.textContent = `${playerPoints} -- ${computerPoints}`
     }, 3000)
 }
-
