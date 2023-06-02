18a365a7a1a6abd1c433138bd87c1d8b03f2297f
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-06-02 15:39:28 +0800

    Rework Code By Using Element Id Selectors For Event Listeners And Selectors

--- javascript.js
+++ javascript.js
@@ -3,26 +3,48 @@ let currentCChoice;
 let currentPChoice;
 let playerPoints = 0;
 let computerPoints = 0;
-const playerChoiceText = document.querySelector('.player-choice.text');
-const resultText = document.querySelector('.result.text');
-const computerChoiceText = document.querySelector('.computer-choice.text');
-const tally = document.querySelector('.tally.text');
-
-//Player Choice event listener, Main Function
+const playerChoiceText = document.getElementById('player-choice');
+const computerChoiceText = document.getElementById('computer-choice');
+const resultText = document.getElementById('result');
+const tally = document.getElementById('tally');
 const playerChoices = document.querySelectorAll('.left-column > .box');
-playerChoices.forEach(btn => btn.addEventListener('click', round));
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorBtn = document.getElementById('scissorBtn');
 
-//Full Game
+rockBtn.addEventListener('click', () => round('Rock'));
+paperBtn.addEventListener('click', () => round('Paper'));
+scissorBtn.addEventListener('click', () => round('Scissor'));
 
 //1 Round
 function round() {
-    playerChoices.forEach(btn => btn.removeEventListener('click', round));
-    currentPChoice = this; //Current Player div
-    currentCChoice = randomComputer(); //Current Computer div
-    textPCRT();
-    resetAll();
+
 }
 
+/*
+//text & highlight functions
+function textPCRT() {
+    playerChoiceText.textContent = `You chose ${currentPChoice}`;
+    currentPChoice.classList.add('highlight');
+    setTimeout(function() {
+        currentCChoice.classList.add('highlight');
+        computerChoiceText.textContent = `The Computer chose ${currentCChoice.dataset.typeCpu}`;
+    }, 1500)
+    setTimeout(function() {
+        if (parseInt(currentPChoice.dataset.value) == parseInt(currentCChoice.dataset.valueCpu)) {
+            resultText.textContent = 'Its A Tie!';
+        } else if (parseInt(currentPChoice.dataset.value) - parseInt(currentCChoice.dataset.valueCpu) == 1 || parseInt(currentPChoice.dataset.value) - parseInt(currentCChoice.dataset.valueCpu) == -2) {
+            resultText.textContent = `${currentPChoice.dataset.type} beats ${currentCChoice.dataset.typeCpu}, You Win!`;
+            playerPoints += 1;
+        } else {
+            resultText.textContent = `${currentCChoice.dataset.typeCpu} beats ${currentPChoice.dataset.type}, You Lose!`;
+            computerPoints += 1;
+        }
+    }, 2800)
+    setTimeout(function() {  
+        tally.textContent = `${playerPoints} -- ${computerPoints}`
+    }, 4000)
+}
 //Calculations; Computer And Results
 function randomComputer() {
     let randomN = Math.floor(Math.random() * 3);
@@ -59,5 +81,5 @@ function resetAll() {
         currentCChoice.classList.remove('highlight');
         computerChoiceText.textContent = '';
         resultText.textContent = '';
-    }, 8000)
+    }, 7500)
 }
